5aba60a98005932c5b99ac25244b47b5d4b65cda
Quoc Huan <phamquochuan9876@gmail.com>
2025-05-08 00:27:41 +0700

    7/5

--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -56,4 +56,8 @@ public class UserService {
 
     }
 
+    public boolean checkEmailExist(String email) {
+        return this.userRepository.exisexistsByEmail(email);
+    }
+
 }
--- src/main/java/vn/hoidanit/laptopshop/service/validator/RegisterValidator.java
+++ src/main/java/vn/hoidanit/laptopshop/service/validator/RegisterValidator.java
@@ -1,10 +1,20 @@
 package vn.hoidanit.laptopshop.service.validator;
 
+import org.springframework.stereotype.Service;
+
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import vn.hoidanit.laptopshop.domain.dto.RegisterDTO;
+import vn.hoidanit.laptopshop.service.UserService;
 
+@Service
 public class RegisterValidator implements ConstraintValidator<RegisterChecked, RegisterDTO> {
+
+    private final UserService userService;
+
+    public RegisterValidator(UserService userService) {
+        this.userService = userService;
+    }
 
     @Override
     public boolean isValid(RegisterDTO user, ConstraintValidatorContext context) {
@@ -12,7 +22,7 @@ public class RegisterValidator implements ConstraintValidator<RegisterChecked, R
 
         // Check if password fields match
         if (!user.getPassword().equals(user.getConfirmPassword())) {
-            context.buildConstraintViolationWithTemplate("Passwords must match")
+            context.buildConstraintViolationWithTemplate("Passwords không chính xác")
                     .addPropertyNode("confirmPassword")
                     .addConstraintViolation()
                     .disableDefaultConstraintViolation();
@@ -20,7 +30,15 @@ public class RegisterValidator implements ConstraintValidator<RegisterChecked, R
         }
 
         // Additional validations can be added here
+        // check email
+        if (this.userService.checkEmailExist(user.getEmail())) {
+            context.buildConstraintViolationWithTemplate("Email đã tồn tại")
+                    .addPropertyNode("email")
+                    .addConstraintViolation()
+                    .disableDefaultConstraintViolation();
+            valid = false;
 
+        }
         return valid;
     }
 }
