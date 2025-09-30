111df4638478ef4fc0484424ea03d4443d9d3d84
Quoc Huan <phamquochuan9876@gmail.com>
2025-09-30 16:42:42 +0700

    30/9/2025

--- src/main/java/vn/hoidanit/laptopshop/controller/client/ItemPageController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/client/ItemPageController.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import vn.hoidanit.laptopshop.domain.Product;
 import vn.hoidanit.laptopshop.service.ProductService;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class ItemPageController {
@@ -29,8 +31,13 @@ public class ItemPageController {
     }
 
     @PostMapping("/add-product-to-cart/{id}")
-    public String addProductToCart(@PathVariable long id) {
-        // TODO: process POST request
+    public String addProductToCart(@PathVariable long id, HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+
+        long productId = id;
+        String email = (String) session.getAttribute("email");
+
+        this.productService.handleAddProductTocart(email, productId);
 
         return "redirect:/";
     }
--- src/main/java/vn/hoidanit/laptopshop/service/ProductService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/ProductService.java
@@ -5,7 +5,10 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import vn.hoidanit.laptopshop.domain.Cart;
+import vn.hoidanit.laptopshop.domain.CartDetail;
 import vn.hoidanit.laptopshop.domain.Product;
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.repository.CartDetailRepository;
 import vn.hoidanit.laptopshop.repository.CartRepository;
 import vn.hoidanit.laptopshop.repository.ProductRepository;
@@ -15,12 +18,17 @@ public class ProductService {
     private final ProductRepository productRepository;
     private final CartRepository cartRepository;
     private final CartDetailRepository cartDetailRepository;
+    private final UserService userService;
 
-    public ProductService(ProductRepository productRepository, CartRepository cartRepository,
-            CartDetailRepository cartDetailRepository) {
+    public ProductService(
+            ProductRepository productRepository,
+            CartRepository cartRepository,
+            CartDetailRepository cartDetailRepository,
+            UserService userService) {
         this.productRepository = productRepository;
         this.cartDetailRepository = cartDetailRepository;
         this.cartRepository = cartRepository;
+        this.userService = userService;
     }
 
     public Product createProduct(Product pr) {
@@ -39,8 +47,38 @@ public class ProductService {
         this.productRepository.deleteById(id);
     }
 
-    public void handleAddProductTocart() {
+    public void handleAddProductTocart(String email, long productId) {
 
+        User user = this.userService.getUserByEmail(email);
+        if (user != null) {
+            // check user đã có cart chưa? nếu chưa -> tạo mới
+            Cart cart = this.cartRepository.findByUser(user);
+
+            if (cart == null) {
+                // tạo mới cart
+                Cart otherCart = new Cart();
+                otherCart.setUser(user);
+                otherCart.setSum(1);
+
+                cart = this.cartRepository.save(otherCart);
+            }
+
+            // save cart_detail
+            // tìm product by id
+
+            Optional<Product> producOptional = this.productRepository.findById(productId);
+            if (producOptional.isPresent()) {
+                Product realProduct = producOptional.get();
+
+                CartDetail cd = new CartDetail();
+                cd.setCart(cart);
+                cd.setProduct(realProduct);
+                cd.setPrice(realProduct.getPrice());
+                cd.setQuantity(1);
+
+                this.cartDetailRepository.save(cd);
+            }
+        }
     }
 
 }
