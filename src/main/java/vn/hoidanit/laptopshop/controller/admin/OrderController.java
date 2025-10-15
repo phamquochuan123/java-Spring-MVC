b2df51c6cbe6d05bf660076173a2d81087a61043
Quoc Huan <phamquochuan9876@gmail.com>
2025-10-15 20:21:06 +0700

    15/10/2025

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/OrderController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/OrderController.java
@@ -3,12 +3,17 @@ package vn.hoidanit.laptopshop.controller.admin;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 import vn.hoidanit.laptopshop.domain.Order;
 import vn.hoidanit.laptopshop.service.OrderService;
 
@@ -22,9 +27,29 @@ public class OrderController {
     }
 
     @GetMapping("/admin/order")
-    public String getDashboard(Model model) {
-        List<Order> orders = this.orderService.fetchAllOrders();
-        model.addAttribute("orders", orders);
+    public String getDashboard(Model model,
+            @RequestParam("page") Optional<String> pageOptional) {
+        // page = 1 . limit = 10
+
+        // duoi db co 10 rows .count =100 =>chia limit = 10 pages
+        int page = 1;
+        try {
+            if (pageOptional.isPresent()) {
+                page = Integer.parseInt(pageOptional.get());
+            } else {
+                // page = 1
+            }
+        } catch (Exception e) {
+            // page = 1
+            // Todo: handle exception
+        }
+        Pageable pageable = PageRequest.of(page - 1, 2);
+        Page<Order> order = this.orderService.fetchAllOrders(pageable);
+        List<Order> listOrders = order.getContent();
+        model.addAttribute("orders", listOrders);
+
+        model.addAttribute("currentPage", page);
+        model.addAttribute("totalPages", order.getTotalPages());
         return "admin/order/show";
     }
 
--- src/main/java/vn/hoidanit/laptopshop/service/OrderService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/OrderService.java
@@ -3,10 +3,13 @@ package vn.hoidanit.laptopshop.service;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import vn.hoidanit.laptopshop.domain.Order;
 import vn.hoidanit.laptopshop.domain.OrderDetail;
+import vn.hoidanit.laptopshop.domain.Product;
 import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.repository.OrderDetailRepository;
 import vn.hoidanit.laptopshop.repository.OrderRepository;
@@ -23,8 +26,8 @@ public class OrderService {
         this.orderRepository = orderRepository;
     }
 
-    public List<Order> fetchAllOrders() {
-        return this.orderRepository.findAll();
+    public Page<Order> fetchAllOrders(Pageable page) {
+        return this.orderRepository.findAll(page);
     }
 
     public Optional<Order> fetchOrderById(long id) {
